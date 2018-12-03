f3855ccdb6928dfb655758b2ac3dbe34474586af
Guillaume Latour <guillaume@cblue.be>
2018-12-03 18:33:11 +0100

    Problem3 - change type of function is.. + implement ErdosGallai solution

--- src/main/java/be/unamur/info/algo2/Problem3/Graph.java
+++ src/main/java/be/unamur/info/algo2/Problem3/Graph.java
@@ -4,17 +4,23 @@ import java.util.Arrays;
 
 public class Graph {
     private int[] degrees;
+    private int[] ordered_degrees;
 
     public Graph(int[] d) {
         this.degrees = d;
+        this.ordered_degrees = null;
     }
 
-    public int isNaiveGraphic() {
+    /**
+     * This function will check if the given 'this.degrees' array may be assimilated to a serie of simple graph's degree.
+     *
+     * @return
+     */
+    public boolean isNaiveGraphic() {
         // at first, check if the degree's sum is even
         if(!this.isSumEven()) {
-            return 0;
+            return false;
         }
-
         int i = this.degrees.length - 1;
         while(i > 0) {
             // a O(n log(n)) sort from the documentation
@@ -22,7 +28,7 @@ public class Graph {
 
             int value = 0;
             if(this.degrees[i]<0) {
-                return 0;
+                return false;
             } else if(this.degrees[i] == 0) {
                 i--;
                 continue;
@@ -32,16 +38,56 @@ public class Graph {
             }
             for(int j = 1; j <= value; j++) {
                 if(i-j < 0) {
-                    return 0;
+                    return false;
                 }
                 this.degrees[i-j]--;
             }
         }
-        return 1;
+        return true;
     }
 
-    public int isGraphic() {
-        return 1;
+
+    public boolean isGraphic() {
+        return false;
+    }
+
+    /**
+     * This is the Erdös Gallai way to solve this problem
+     *
+     * @param k
+     * @return
+     */
+    public boolean solveEG(int k) {
+        if(k == this.getDegreeSize() && this.ordered_degrees == null) {
+            this.ordered_degrees = this.degrees;
+            Arrays.sort(this.ordered_degrees);
+        }
+
+        if(k < 1) {
+            return true;
+        }
+
+        int sum_degree = 0;
+        for(int i = 0; i < k; i++) {
+            sum_degree+=this.ordered_degrees[i];
+        }
+
+        if(k == this.ordered_degrees.length && sum_degree %2 != 0) {
+            return false;
+        }
+
+        int f = k*(k-1);
+
+        int sum_right = 0;
+        for(int i = k; i < this.ordered_degrees.length; i++) {
+            sum_right+= Math.min(k+1, this.ordered_degrees[i]);
+        }
+
+        if(sum_degree > f + sum_right) {
+            return false;
+        }
+
+        return solveEG(k-1);
     }
 
     /**
@@ -56,4 +102,7 @@ public class Graph {
         return sum%2 == 0;
     }
 
+    public int getDegreeSize() {
+        return this.degrees.length;
+    }
 }
