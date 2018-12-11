1458c95ce5ab09c7d17febc58855e4a8629403eb
Guillaume Latour <guillaume@cblue.be>
2018-12-11 21:39:21 +0100

    Problem 2 - adding some tests

--- src/main/java/be/unamur/info/algo2/Problem2/Problem2.java
+++ src/main/java/be/unamur/info/algo2/Problem2/Problem2.java
@@ -16,17 +16,24 @@ public class Problem2 {
         
     	String[] lines = content.split("\n");
         int number_of_lines = lines.length;
-        int count_of_lines = 1; // since the first line is always read to know the number of grid
         if(number_of_lines < 3) {
             throw new Exception("Not enough line for most trivial case");
         }
-        
+
+        // check that first line is positive integer
+        if(!lines[0].matches("^\\d+$")) {
+            throw new Exception("Information must be an integer");
+        }
         try {
-        number_of_grid = Integer.valueOf(lines[0]);
+            number_of_grid = Integer.valueOf(lines[0]);
         } catch (Exception e) {
         	throw new Exception("not a parsable number of grid");
         }
-        if (number_of_grid == 0 ) throw new Exception("Not a valid number of grid");
+        if (number_of_grid <= 0 ) {
+            throw new Exception("Not a valid number of grid");
+        }
+
+        //
         this.grids = new Grid[number_of_grid];
         int count_of_grid = 0;
 
@@ -36,25 +43,34 @@ public class Problem2 {
         boolean next_is_new_grid = true;
         int[] size = new int[2];
         String[] str_size;
-        String[] line_content = new String[0];        
+        String[] line_content = new String[0];
         for(int i = 1; i < number_of_lines; i++) {
-            count_of_lines++;
             if(next_is_new_grid) {
+                if(!lines[i].matches("^\\d+\\s\\d+$")) {
+                    throw new Exception("Grid does not respect size definition");
+                }
                 str_size = lines[i].split(" ");
                 try {
-                	size[0] = Integer.parseInt(str_size[0]); // line
-                	size[1] = Integer.parseInt(str_size[1]); // column
+                	size[0] = Integer.valueOf(str_size[0]); // line
+                	size[1] = Integer.valueOf(str_size[1]); // column
                 } catch (Exception e) {
                 	throw new Exception("not a parsable number of grid line or col");
                 } 
-                if (size[0] == 0 || size[1] == 0) throw new Exception("Grid "+count_of_grid+" is not a valid Grid");
+                if (size[0] <= 0 || size[1] <= 0) {
+                    throw new Exception("Grid "+count_of_grid+" is not a valid Grid");
+                }
                 next_is_new_grid = false;
                 number_of_line_in_grid = size[0];
                 count_of_line_in_grid = 0;
                 line_content = new String[size[1]];
                 continue;
             }
-
+            if(!lines[i].matches("^(0|T|#)+$")) {
+                throw new Exception("Line " + i + " contains illegal characters (" + lines[i] + ")");
+            }
+            if(lines[i].toCharArray().length != line_content.length) {
+                throw new Exception("Line " + i + " does not respect the characters count");
+            }
             line_content[count_of_line_in_grid] = lines[i];
             count_of_line_in_grid++;
             // fill the lines array
